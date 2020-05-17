c9bedda3660e512b008145b8e91ac02ed0406538
RoelofRuis <roelof.ruis@gmail.com>
2020-05-17 15:02:58 +0200

    little button separation

--- src/main/scala/app/view/Board.scala
+++ src/main/scala/app/view/Board.scala
@@ -8,18 +8,23 @@ import scalatags.Text.all._
 object Board {
 
   def view(game: Game): Text.TypedTag[String] = {
-    val board = div(cls := "row")(
-      div(cls := "col-md-8 mx-auto")(
-        canvas(id := "board", widthA := 600, heightA := 600),
-      )
+    val board = div(cls := "col-md-8 mx-auto")(
+      canvas(id := "board", widthA := 600, heightA := 600),
     )
 
     val controls = game.state match {
-      case model.Mahjong.NewGame => button(cls := "btn btn-outline-primary", onclick := "Mahjong.next()")("Start game")
+      case model.Mahjong.NewGame => div(cls := "col-md8")(
+        button(cls := "btn btn-outline-primary", onclick := "Mahjong.next()")("Start game")
+      )
       case _ => div()
     }
 
-    div(board, controls)
+    div(
+      hr(),
+      div(cls := "row")(board),
+      hr(),
+      div(cls := "row")(controls),
+    )
   }
 
   def draw(game: Game): Unit = {
